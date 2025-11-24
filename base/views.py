cd846c56bacd945081bd0a837132d82b5cba395a
dimah <dimah.code@gmail.com>
2025-11-24 15:48:14 +0330

    feat: add migrations to base/migrations/

--- base/views.py
+++ base/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from .models import Room
 
 
 def home(request):
-    return HttpResponse('Home page')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse("Room")
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
