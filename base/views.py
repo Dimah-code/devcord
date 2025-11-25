6a2d4bdf7e4717bccbe8b76795a971e2e01d7377
dimah <dimah.code@gmail.com>
2025-11-25 11:02:58 +0330

    feat: add query filtering to home function in views.py

--- base/views.py
+++ base/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import redirect, render
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
-    rooms = Room.objects.filter(topic__name__contains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__contains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+    room_count = rooms.count()
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics': topics}
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
 
     return render(request, 'base/home.html', context)
 
