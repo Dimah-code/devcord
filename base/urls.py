020b5b0d01d8bf047f83e911fbc1618331853b4a
dimah <dimah.code@gmail.com>
2025-11-27 12:02:53 +0330

    feat: add user profile url to urls.py

--- base/urls.py
+++ base/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-
     path('room/<str:pk>/', views.room, name='room'),
+    path('profile/<str:pk>/', views.user_profile, name='user-profile'),
+
     path('create-room/', views.create_room, name='create-room'),
     path('update-room/<str:pk>', views.update_room, name='update-room'),
     path('delete-room/<str:pk>', views.delete_room, name='delete-room'),
@@ -13,5 +14,5 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('logout/', views.logout_user, name='logout'),
 
-    path('delete-message/<pk:str>/', views.delete_message, name='delete-message'),
+    path('delete-message/<str:pk>/', views.delete_message, name='delete-message'),
 ]
