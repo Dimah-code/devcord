26a339ab7c52540e8489e1fd5ebd6d6f5270b69c
dimah <dimah.code@gmail.com>
2025-12-04 14:22:14 +0330

    feat: add api/ to base/

--- Devcord/urls.py
+++ Devcord/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('base.urls'))
+    path('', include('base.urls')),
+    path('api/', include('base.api.urls')),
+
 ]
