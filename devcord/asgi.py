84b6af03e22552faacc1a7405356be091c225cd2
dimah <dimah.code@gmail.com>
2025-12-05 10:07:52 +0330

    feat: rename DevCord to devcord

--- devcord/asgi.py
+++ devcord/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Devcord project.
+ASGI config for devcord project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Devcord.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devcord.settings")
 
 application = get_asgi_application()
--- devcord/wsgi.py
+++ devcord/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Devcord project.
+WSGI config for devcord project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Devcord.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devcord.settings")
 
 application = get_wsgi_application()
